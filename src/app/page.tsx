a36969e461ebc3b5e00df55b55447ae251cf42ae
Adrian Oduma <98812231+Odushhh@users.noreply.github.com>
2025-10-06 12:59:31 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,8 @@ export default function Home() {
   // Add images of selected works here (excluding .DS_Store and videos)
   const carouselImages = [    
     '/images/exness1.png',
+    '/images/wrn01.png',
+    '/images/wrn02.png',
     '/images/astral1.png',
     '/images/eventapp3.png',
     '/images/chpter02.png',
