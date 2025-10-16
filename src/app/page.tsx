6cc746f6ccd17e3055a5721e3faa426d1601f8ad
Adrian Oduma <98812231+Odushhh@users.noreply.github.com>
2025-10-16 18:52:36 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,11 +105,11 @@ export default function Home() {
                 
               </div>
               {/* Headline */}
-              <h1 className="text-xl md:text-xl lg:text-[1.25rem] xl:text-[1.5rem] font-semibold lg:font-semibold xl:font-semibold text-[#E5E5E5] mb-4 lg:mb-6 tracking-normal md:tracking-normal lg:tracking-normal xl:tracking-normal">Design studio for Web3<br />and AI startups.</h1>
+              <h1 className="text-xl md:text-xl lg:text-[1.25rem] xl:text-[1.5rem] font-semibold lg:font-semibold xl:font-semibold text-[#E5E5E5] mb-4 lg:mb-6 tracking-normal md:tracking-normal lg:tracking-normal xl:tracking-normal">Design partner for Web3<br />and AI startups.</h1>
               {/* Subtext */}
               <div className="text-sm md:text-sm lg:text-sm xl:text-sm space-y-4 text-gray-400 mb-1 md:mb-8 lg:mb-8 xl:mb-8">
                 <p>Crafting digital experiences that look good, but work even better.</p>
-                <p>Whether you're launching from 0 → 1 or scaling what's working, design is handled with intent & speed.</p>
+                <p>Whether you're launching from 0 → 1 or scaling what's working, I handle design with intent & speed.</p>
                 
                 {/*
                 <p>Startups, founders, and brands trust us to lead their product design efforts from 0→1.</p>
@@ -154,9 +154,10 @@ export default function Home() {
               
               {/* Client Logos */}
               <div>
-                <p className="text-gray-400 text-sm mb-3 md:mb-2">Previous partners:</p>
+                <p className="text-gray-400 text-sm mb-3 md:mb-2">Previous partners I've worked with:</p>
                 <div className="flex flex-wrap gap-x-4 gap-y-4 items-center opacity-80 py-2 md:mb-2 lg:mb-2 xl:mb-2">
                   <Image src="/mookhlogo.png" alt="mookh" width={85} height={110} className="w-auto h-3 lg:h-4 xl:h-5 opacity-70" />
+                  <Image src="/astrallogo.png" alt="astral" width={135} height={110} className="w-auto h-3 lg:h-4 xl:h-5" />
                   <Image src="/chpterLogo.png" alt="chpter" width={135} height={110} className="w-auto h-3 lg:h-4 xl:h-5" />
                   <Image src="/wellthilogo.png" alt="wellthi" width={95} height={110} className="w-auto h-2 lg:h-4 xl:h-5" />
                   <Image src="/wrnlogo.png" alt="we run nairobi" width={135} height={110} className="w-auto h-3 lg:h-4 xl:h-5" />
