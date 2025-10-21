8ef9ed09a9854b642b8764ed895e2f408891a8c7
Adrian Oduma <98812231+Odushhh@users.noreply.github.com>
2025-10-21 09:52:30 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,9 +108,9 @@ export default function Home() {
               <h1 className="text-xl md:text-xl lg:text-[1.25rem] xl:text-[1.5rem] font-semibold lg:font-semibold xl:font-semibold text-[#E5E5E5] mb-4 lg:mb-6 tracking-normal md:tracking-normal lg:tracking-normal xl:tracking-normal">Design partner for Web3<br />and AI startups.</h1>
               {/* Subtext */}
               <div className="text-sm md:text-sm lg:text-sm xl:text-sm space-y-4 text-gray-400 mb-1 md:mb-8 lg:mb-8 xl:mb-8">
-                <p>Ran solo by Adrian Oduma.</p>
                 <p>Crafting digital experiences that look good, but work even better.</p>
                 <p>Whether you're launching from 0 → 1 or scaling what's working, I handle design with intent & speed.</p>
+                <p>Ran solo by Adrian Oduma.</p>
                 
                 {/*
                 <p>Startups, founders, and brands trust us to lead their product design efforts from 0→1.</p>
