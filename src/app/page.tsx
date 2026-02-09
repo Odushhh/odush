ce824f1bdbd56e9a6e740147c282d3c3bd3d855e
Adrian Oduma <98812231+Odushhh@users.noreply.github.com>
2026-02-09 23:38:01 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
               </div>
 
               <br></br><br></br><br></br>
-              <p className="text-white text-base md:text-lg lg:text-[1rem] xl:text-[1.15rem]"><a href="https://adrian-oduma.framer.website/"></a>Adrian Oduma's Design Portfolio</a></p>
+              <p className="underlined decoration-1 text-white text-base md:text-lg lg:text-[1rem] xl:text-[1.15rem]"><a href="https://adrian-oduma.framer.website/"></a>Adrian Oduma's Design Portfolio</a></p>
 
             </div>
             
