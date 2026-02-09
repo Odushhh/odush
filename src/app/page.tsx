ece4cd3d5d47c186c35328d13531e1fad1fcb7c2
Adrian Oduma <98812231+Odushhh@users.noreply.github.com>
2026-02-09 23:37:02 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,12 +160,12 @@ export default function Home() {
               {/* Client Logos */}
               <div>
                 <p className="text-gray-400 text-sm mb-8 md:mb-4 sm:mb-4 lg:mb-8 xl:mb-8">Previous clients I've worked with:</p>
-                <div className="flex flex-wrap gap-x-4 gap-y-4 items-center opacity-80 py-2 md:mb-2 lg:mb-2 xl:mb-2">
-                  <Image src="/mookhlogo.png" alt="mookh" width={85} height={110} className="w-auto h-3 lg:h-3 xl:h-4 opacity-80" />
-                  <Image src="/astrallogo.png" alt="astral" width={135} height={110} className="w-auto h-3 lg:h-3 xl:h-4" />
-                  <Image src="/chpterLogo.png" alt="chpter" width={135} height={110} className="w-auto h-3 lg:h-3 xl:h-4" />
-                  <Image src="/wellthilogo.png" alt="wellthi" width={95} height={110} className="w-auto h-2 lg:h-3 xl:h-4" />
-                  <Image src="/wrnlogo.png" alt="we run nairobi" width={135} height={110} className="w-auto h-3 lg:h-3 xl:h-4" />
+                <div className="flex flex-wrap gap-x-6 gap-y-4 items-center opacity-80 py-2 md:mb-2 lg:mb-2 xl:mb-2">
+                  <Image src="/mookhlogo.png" alt="mookh" width={85} height={110} className="w-auto h-4 lg:h-4 xl:h-4 opacity-80" />
+                  <Image src="/astrallogo.png" alt="astral" width={135} height={110} className="w-auto h-4 lg:h-4 xl:h-4" />
+                  <Image src="/chpterLogo.png" alt="chpter" width={135} height={110} className="w-auto h-4 lg:h-4 xl:h-4" />
+                  <Image src="/wellthilogo.png" alt="wellthi" width={95} height={110} className="w-auto h-3 lg:h-4 xl:h-4" />
+                  <Image src="/wrnlogo.png" alt="we run nairobi" width={135} height={110} className="w-auto h-4 lg:h-4 xl:h-4" />
                 </div>
                 
               </div>
